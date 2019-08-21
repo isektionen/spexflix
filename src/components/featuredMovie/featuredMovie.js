cdaf17583e33e26164cde90146385ce83cb2aa3c
Viktor <viktor@intrykk.se>
2019-08-21 02:18:23 +0200

    Add Button component

--- src/components/featuredMovie/featuredMovie.js
+++ src/components/featuredMovie/featuredMovie.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Button } from '../../components';
+
 import styles from './featuredMovie.module.scss';
 
 const FeaturedMovie = ({ synopsis, title }) => (
   <section className={styles.featuredMovie}>
     <div className={styles.movieDetails}>
       <p className={styles.title}>{title}</p>
+      <div className={styles.buttons}>
+        <Button grower={true} text="Play" />
+        <Button text="Add to my list" />
+      </div>
       <p className={styles.synopsis}>{synopsis}</p>
     </div>
   </section>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,15 @@
+import Button from './button/button';
 import FeaturedMovie from './featuredMovie/featuredMovie';
 import Footer from './footer/footer';
 import Header from './header/header';
 import HorizontalMovieScroller from './horizontalMovieScroller/horizontalMovieScroller';
 import Layout from './layout/layout';
 
-export { FeaturedMovie, Footer, Header, HorizontalMovieScroller, Layout };
+export {
+  Button,
+  FeaturedMovie,
+  Footer,
+  Header,
+  HorizontalMovieScroller,
+  Layout,
+};
