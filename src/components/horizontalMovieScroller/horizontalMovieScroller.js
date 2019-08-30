77b5164deec988fa76087da3369f6e6143a7743c
Viktor <viktor@intrykk.se>
2019-08-30 10:51:23 +0200

    Fetch all data from App.js

--- src/components/horizontalMovieScroller/horizontalMovieScroller.js
+++ src/components/horizontalMovieScroller/horizontalMovieScroller.js
@@ -1,33 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Item from './item';
 
 import styles from './horizontalMovieScroller.module.scss';
 
-import { getPlaylistItems } from '../../api/youtube';
-
-const HorizontalMovieScroller = ({ playlistId, title }) => {
-  const [playlistItems, setPlaylistItems] = useState([]);
-  const [playlistItemsHasLoaded, setPlaylistItemsHasLoaded] = useState(false);
-
-  const itemComponents = playlistItems.map((item, index) => (
-    <Item
-      key={index}
-      duration="unknown"
-      hasLoaded={playlistItemsHasLoaded}
-      tags={item.tags}
-      thumbnails={item.thumbnails}
-      title={item.title}
-    />
-  ));
-
-  useEffect(() => {
-    getPlaylistItems(playlistId).then(items => {
-      setPlaylistItems(items);
-      setPlaylistItemsHasLoaded(true);
-    });
-  }, [playlistId]);
+const HorizontalMovieScroller = ({ items, title }) => {
+  const itemComponents = items
+    ? items.map((item, index) => (
+        <Item
+          key={index}
+          duration="unknown"
+          tags={item.tags}
+          thumbnails={item.thumbnails}
+          title={item.title}
+        />
+      ))
+    : null;
 
   return (
     <div className={styles.horizontalMovieScroller}>
@@ -45,7 +34,7 @@ const HorizontalMovieScroller = ({ playlistId, title }) => {
 };
 
 HorizontalMovieScroller.propTypes = {
-  playlistId: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
   title: PropTypes.string.isRequired,
 };
 
