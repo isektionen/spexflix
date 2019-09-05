08061708a557a9a589db1405df7f85058c4ce67c
Viktor <viktor@intrykk.se>
2019-09-05 14:16:28 +0200

    Fix logotype slope for names with uneven length

--- src/components/logotype/logotype.tsx
+++ src/components/logotype/logotype.tsx
@@ -11,15 +11,25 @@ const Logotype = ({ text }: Props) => {
   const maxOffset = Math.floor(letters.length / 2);
   const slope = 10;
 
+  const evenLength = letters.length % 2 === 0 ? true : false;
+
   const letterComponents = letters.map((letter, index) => {
-    const offset = index + 1 - Math.ceil(letters.length / 2);
+    const offset = evenLength
+      ? index <= maxOffset - 1
+        ? index - maxOffset
+        : index - maxOffset + 1
+      : index + 1 - Math.ceil(letters.length / 2);
     const angle = (-offset / maxOffset) * slope;
     const scale = 1 + Math.pow(Math.abs(offset), 2) * 0.01;
 
     return (
       <div>
         <a href="/">
-          <span style={{ transform: `rotateY(${angle}deg) scaleY(${scale})` }}>
+          <span
+            style={{
+              transform: `rotateY(${angle}deg) scaleY(${scale})`,
+            }}
+          >
             {letter}
           </span>
         </a>
