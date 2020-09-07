38d58bb8bbb5f1adefc33bbce07f11aa44e5e567
Viktor Mörsell <viktor.morsell@northvolt.com>
2020-09-07 23:11:09 +0200

    Use Netflix-style scrolling

---
+++ components/icon/chevron.tsx
@@ -0,0 +1,40 @@
+export interface ChevronProps {
+  direction?: 'up' | 'down' | 'left' | 'right'
+  size?: number
+  fill?: string
+}
+
+const Chevron = ({
+  direction = 'right',
+  size = 24,
+  fill = '#000',
+}: ChevronProps) => {
+  let rotation = 0
+  switch (direction) {
+    case 'up':
+      rotation = -90
+      break
+    case 'down':
+      rotation = 90
+      break
+    case 'left':
+      rotation = 180
+      break
+  }
+
+  return (
+    <svg
+      width={size}
+      height={size}
+      style={{ transform: `rotateZ(${rotation}deg)` }}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill={fill}
+        d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
+      />
+    </svg>
+  )
+}
+
+export default Chevron
---
+++ components/icon/index.ts
@@ -0,0 +1,7 @@
+import Chevron from './chevron'
+
+const Icon = {
+  Chevron,
+}
+
+export default Icon
