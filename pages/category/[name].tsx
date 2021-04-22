9cbf0fc84e91caddf51433150e03f3ae1afd624d
Viktor Mörsell <viktor.morsell@northvolt.com>
2021-04-22 02:31:31 +0200

    Add show category pages

---
+++ pages/category/[name].tsx
@@ -0,0 +1,76 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
+import graphcms, { gql } from '../../lib/graphcms'
+
+import Layout from '../../components/layout'
+import PlaylistScroller from '../../components/playlistScroller'
+import FeaturedVideo from '../../components/featuredVideo'
+
+const ShowTypePage = ({ shows, featured, categories }): JSX.Element => (
+  <Layout
+    title={process.env.NEXT_PUBLIC_SITE_TITLE}
+    copyrightFromYear={process.env.NEXT_PUBLIC_COPYRIGHT_FROM_YEAR}
+    publisher={process.env.NEXT_PUBLIC_PUBLISHER}
+    categories={categories}
+  >
+    {featured && <FeaturedVideo show={featured} />}
+    {shows.map((s) => (
+      <PlaylistScroller key={s.id} show={s} />
+    ))}
+  </Layout>
+)
+export default ShowTypePage
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { shows, categories } = await graphcms.request(
+    gql`
+      query CategoryPage($name: Category!) {
+        shows(where: { category: $name }) {
+          title
+          orTitle
+          videos {
+            slug
+            title
+          }
+        }
+        categories: __type(name: "Category") {
+          enumValues {
+            name
+          }
+        }
+      }
+    `,
+    {
+      name: context.params.name,
+    }
+  )
+
+  return {
+    props: {
+      shows,
+      categories: categories.enumValues.map((v) => v.name),
+    },
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { categories } = await graphcms.request(
+    gql`
+      {
+        categories: __type(name: "Category") {
+          enumValues {
+            name
+          }
+        }
+      }
+    `
+  )
+
+  return {
+    paths: categories.enumValues.map((v) => ({
+      params: {
+        name: v.name,
+      },
+    })),
+    fallback: false,
+  }
+}
