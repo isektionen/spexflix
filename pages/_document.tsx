1e6ad40c956d95906b43e838089515e3022bef4e
Viktor Mörsell <viktor.morsell@northvolt.com>
2021-04-29 23:39:26 +0200

    Inject Segment analytics script

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import * as snippet from '@segment/snippet'
+
+export default class MyDocument extends Document {
+  renderSnippet() {
+    const opts = {
+      apiKey: process.env.SEGMENT_WRITE_KEY,
+      // note: the page option only covers SSR tracking.
+      // Page.js is used to track other events using `window.analytics.page()`
+      page: true,
+    }
+
+    if (process.env.NEXT_PUBLIC_ENV === 'dev') {
+      return snippet.max(opts)
+    }
+
+    return snippet.min(opts)
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Inject the Segment snippet into the <head> of the document  */}
+          <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
