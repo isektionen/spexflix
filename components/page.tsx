183f2a278b1ef6436a35047370a29be34f33dbdd
Viktor Mörsell <viktor.morsell@northvolt.com>
2021-04-29 23:37:49 +0200

    Track page views

---
+++ components/page.tsx
@@ -0,0 +1,9 @@
+import Router from 'next/router'
+
+Router.events.on('routeChangeComplete', (url: string) => {
+  window.analytics.page(url)
+})
+
+const Page = ({ children }) => <div>{children}</div>
+
+export default Page
