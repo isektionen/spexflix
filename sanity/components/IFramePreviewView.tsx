f53e33514518cc22ff0eb991d51b948ec586374a
Viktor Mörsell <viktor@upper.st>
2023-04-11 06:13:20 +0200

    Fix build errors and format

--- sanity/components/IFramePreviewView.tsx
+++ sanity/components/IFramePreviewView.tsx
@@ -1,58 +1,58 @@
-import { Box, Text } from '@sanity/ui'
-import { ComponentProps, Suspense } from 'react'
-import { isRecord, isString, useClient } from 'sanity'
-import { UserViewComponent } from 'sanity/desk'
-import styled from 'styled-components'
-import { suspend } from 'suspend-react'
+import { Box, Text } from '@sanity/ui';
+import { ComponentProps, Suspense } from 'react';
+import { isRecord, isString, useClient } from 'sanity';
+import { UserViewComponent } from 'sanity/desk';
+import styled from 'styled-components';
+import { suspend } from 'suspend-react';
 
-import { apiVersion, previewSecretDocumentId } from '../env'
-import { getPreviewSecret } from '../lib/previewSecret'
+import { apiVersion, previewSecretDocumentId } from '../env';
+import { getPreviewSecret } from '../../src/lib/sanity/previewSecret';
 
-const FETCH_SECRET = Symbol(previewSecretDocumentId)
+const FETCH_SECRET = Symbol(previewSecretDocumentId);
 
 const StyledIframe = styled.iframe`
   border: 0;
   width: 100%;
   height: 100%;
-`
+`;
 
 export function IFramePreviewView(props: ComponentProps<UserViewComponent>) {
   const {
     document: { displayed },
     documentId,
     schemaType,
-  } = props
+  } = props;
 
-  const id = documentId
-  const type = schemaType.name
+  const id = documentId;
+  const type = schemaType.name;
   const slug =
     isRecord(displayed.slug) && isString(displayed.slug.current)
       ? displayed.slug.current
-      : undefined
+      : undefined;
 
   if (!slug) {
     return (
       <Box>
         <Text>Missing slug</Text>
       </Box>
-    )
+    );
   }
 
   return (
     <Suspense fallback={null}>
       <PagePreviewWithSecret id={id} slug={slug} type={type} />
     </Suspense>
-  )
+  );
 }
 
 function PagePreviewWithSecret(props: {
-  id: string
-  slug: string
-  type: string
+  id: string;
+  slug: string;
+  type: string;
 }) {
-  const { id, slug, type } = props
+  const { id, slug, type } = props;
 
-  const client = useClient({ apiVersion })
+  const client = useClient({ apiVersion });
 
   // Use `suspend` to fetch the secret with a TTL of 1 minute, just to check if it's necessary to
   // recreate the secret which has a TTL of 60 minutes.
@@ -65,15 +65,15 @@ function PagePreviewWithSecret(props: {
       }),
     ['getPreviewSecret', previewSecretDocumentId, FETCH_SECRET],
     { lifespan: 60000 }
-  )
+  );
 
   if (!secret) {
-    return <div>No secret</div>
+    return <div>No secret</div>;
   }
 
   return (
     <StyledIframe
       src={`/api/sanity/preview?type=${type}&id=${id}&slug=${slug}&secret=${secret}`}
     />
-  )
+  );
 }
--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,18 +1,18 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import type { Image } from 'sanity'
+import createImageUrlBuilder from '@sanity/image-url';
+import type { Image } from 'sanity';
 
-import { dataset, projectId } from '../env'
+import { dataset, projectId } from '../../../sanity/env';
 
 const imageBuilder = createImageUrlBuilder({
   projectId: projectId || '',
   dataset: dataset || '',
-})
+});
 
 export const urlForImage = (source: Image) => {
   // Ensure that source image contains a valid reference
   if (!source?.asset?._ref) {
-    return undefined
+    return undefined;
   }
 
-  return imageBuilder?.image(source).auto('format').fit('max')
-}
+  return imageBuilder?.image(source).auto('format').fit('max');
+};
