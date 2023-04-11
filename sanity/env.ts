58107a802c283fef4d8a70d4da2a8d4512229846
Viktor Mörsell <viktor@upper.st>
2023-04-11 06:53:03 +0200

    Use latest Sanity API by default

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,7 +1,8 @@
 export const title = process.env.NEXT_PUBLIC_SITE_TITLE || 'Next.js project';
 
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-01-01';
+const d = new Date();
+const today = `${d.getUTCFullYear}-${d.getUTCMonth}-${d.getUTCDate}`;
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || today;
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
